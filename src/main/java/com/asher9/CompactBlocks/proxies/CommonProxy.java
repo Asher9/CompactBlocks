4a11b6fc154a5d7ab41055116bbb2f662af4c71d
Asher9 <baumann.ruben@gmx.de>
2018-05-12 22:13:41 +0200

    Updated Mod from 1.10.2

--- src/main/java/com/asher9/CompactBlocks/proxies/CommonProxy.java
+++ src/main/java/com/asher9/CompactBlocks/proxies/CommonProxy.java
@@ -1,9 +1,12 @@
 package com.asher9.CompactBlocks.proxies;
 
+import com.asher9.CompactBlocks.world.WorldGen;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class CommonProxy {
 
     public void init() {
-
+        GameRegistry.registerWorldGenerator(new WorldGen(), 0);
     }
 
     public void registerRenders() {
