5bc43089bb7f78352e43e6e77960ff4a0e0bb742
Asher9 <baumann.ruben@gmx.de>
2020-01-02 21:34:54 +0100

    d

--- src/main/java/com/example/examplemod/ModEventSubscriber.java
+++ src/main/java/com/example/examplemod/ModEventSubscriber.java
@@ -1,23 +1,38 @@
 package com.example.examplemod;
 
 import com.example.examplemod.init.ModItemGroups;
+import com.google.common.base.Preconditions;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import javax.annotation.Nonnull;
 
 @Mod.EventBusSubscriber(modid = Reference.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
 
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(
+        final IForgeRegistry<Item> registry = event.getRegistry();
+        registry.registerAll(
                 setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "example_item")
         );
+
+        ForgeRegistries.BLOCKS.getValues().parallelStream()
+                .filter(block -> block.getRegistryName().getNamespace().equals(Reference.MODID))
+                .forEach(block -> {
+                    final Item.Properties properties = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
+                    final BlockItem blockItem = new BlockItem(block, properties);
+                    registry.register(setup(blockItem, block.getRegistryName()));
+                });
     }
 
     @SubscribeEvent
@@ -27,11 +42,16 @@ public class ModEventSubscriber {
         );
     }
 
-    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+    @Nonnull
+    public static <T extends IForgeRegistryEntry<T>> T setup(@Nonnull final T entry, @Nonnull final String name) {
+        Preconditions.checkNotNull(name, "Name to assign to entry cannot be null!");
         return setup(entry, new ResourceLocation(Reference.MODID, name));
     }
 
-    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+    @Nonnull
+    public static <T extends IForgeRegistryEntry<T>> T setup(@Nonnull final T entry, @Nonnull final ResourceLocation registryName) {
+        Preconditions.checkNotNull(entry, "Entry cannot be null!");
+        Preconditions.checkNotNull(registryName, "Registry name to assign to entry cannot be null!");
         entry.setRegistryName(registryName);
         return entry;
     }
